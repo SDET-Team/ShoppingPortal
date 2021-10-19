b6b8829878582335a37091785be2545fe4db443d
Nikhil Pawar <nikhilpaward2@gmail.com>
2021-10-19 13:54:19 +0530

    modified order history testcases

--- src/test/java/com/project/testcases/OrderHistoryPageTest.java
+++ src/test/java/com/project/testcases/OrderHistoryPageTest.java
@@ -1,9 +1,10 @@
 package com.project.testcases;
 
+import java.io.IOException;
+
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.project.base.CommonBase;
@@ -12,66 +13,73 @@ import com.project.pages.HomePage;
 import com.project.pages.LoginPage;
 import com.project.pages.MyAccountPage;
 import com.project.pages.OrderHistoryPage;
+import com.project.utils.TestUtils;
 
 public class OrderHistoryPageTest extends CommonBase {
 	
-	LoginPage loginpage;
-	OrderHistoryPage orderHistoryPage;
+	AfterLoginPage afterLoginPage;
 	
 	public OrderHistoryPageTest() {
 		super();
 	}
 	
 	
-	@BeforeClass
+	@BeforeTest
 	public void setup() {
 		initialization();
 		HomePage homepage=new HomePage();
 		Assert.assertEquals(homepage.title(), "Shopping Portal Home Page", "Home Page Title Not Matched.");
 		navbeforeLogin.navigatetologin();
 		
-		loginpage=new LoginPage();
+		LoginPage loginpage=new LoginPage();
 		Assert.assertEquals(loginpage.loginpageTitle(), "Shopping Portal | Signi-in | Signup", "Login Page Title Not Matched.");
+		
+		String file = System.getProperty("user.dir") + "\\src\\resources\\testdata\\myAccountTestData.xlsx";
+		Object[][] data;
+		try {
+			data = TestUtils.getTestData(file, 0);
+			String email = data[0][0].toString();
+			String password = data[0][1].toString();
+			
+			loginpage.loginOperation(email, password);
+			
+			afterLoginPage = new AfterLoginPage();
+			Assert.assertEquals(afterLoginPage.getTitle(), "My Cart");
+		} catch (IOException e) {  }
 	}
 	
-	
-	@Test(priority=1, dataProvider="login_data")
-	public void loginAndNavigateToOrderHistory(String email, String password, String expectedResult) {
-		boolean actualResult = loginpage.loginOperation(email, password, expectedResult);
-		Assert.assertFalse(actualResult);
-		
-		AfterLoginPage afterLoginPage = new AfterLoginPage();
-		Assert.assertEquals(afterLoginPage.getTitle(), "My Cart");
+	@Test
+	public void checkIfNewOrdersAtTopInOrderHistory() {
 		afterLoginPage.goToMyAccountPage();
 		
 		MyAccountPage myAccountPage = new MyAccountPage();
 		Assert.assertEquals(myAccountPage.getTitle(), "My Account");
 		myAccountPage.gotoOrderHistoryPageButton();
 		
-		orderHistoryPage = new OrderHistoryPage();
+		OrderHistoryPage orderHistoryPage = new OrderHistoryPage();
 		Assert.assertEquals(orderHistoryPage.getTitle(), "Order History");
-	}
-	
-	@Test(priority=2)
-	public void checkIfNewOrdersAtTopInOrderHistory() {
+		
 		boolean output = orderHistoryPage.checkLatestOrderIsOnTop();
 		Assert.assertTrue(output, "Order history does not show newer orders at top.");
 	}
 	
-	@Test(priority=2)
+	@Test
 	public void checkIfAllOrdersContainProductDetails() {
+		afterLoginPage.goToMyAccountPage();
+		
+		MyAccountPage myAccountPage = new MyAccountPage();
+		Assert.assertEquals(myAccountPage.getTitle(), "My Account");
+		myAccountPage.gotoOrderHistoryPageButton();
+		
+		OrderHistoryPage orderHistoryPage = new OrderHistoryPage();
+		Assert.assertEquals(orderHistoryPage.getTitle(), "Order History");
+		
 		boolean output = orderHistoryPage.checkAllOrdersContainProductDetails();
 		Assert.assertTrue(output, "One or more orders are missing product details.");
 	}
 	
 	
-	@DataProvider(name="login_data")
-	private Object[][] getLoginDetails() {
-		return new Object[][] { {"anuj.lpu1@gmail.com", "Test@123", "pass"} };
-	}
-	
-	
-	@AfterClass
+	@AfterTest
 	public void tearDown() {
 		//try { Thread.sleep(5*1000); } catch(Exception e) {}
 		driver.quit();
